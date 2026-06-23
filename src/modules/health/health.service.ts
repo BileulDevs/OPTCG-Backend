b51b542b553e6dfa2fc6cf4d5fea68ad75f4ab1f
BileulDevs <bsahraoui.mail@gmail.com>
2026-06-24 00:45:38 +0200

    feat: health module

---
+++ src/modules/health/health.service.ts
@@ -0,0 +1,13 @@
+export interface HealthStatus {
+    status: "ok",
+    uptime: number,
+    timestamp: string
+}
+
+export function getHealthStatus() : HealthStatus {
+    return {
+        status: "ok",
+        uptime: Number(process.uptime().toFixed(2)),
+        timestamp: new Date().toISOString()
+    }
+}
