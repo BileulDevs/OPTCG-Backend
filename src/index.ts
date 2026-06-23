1984176dca60779b79207e656b264c03601d7776
BileulDevs <bsahraoui.mail@gmail.com>
2026-06-23 20:21:33 +0200

    feat: copyfiles lib

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, {Response, Request} from "express";
 require('dotenv').config()
 
+const a = require("@/mod/a.json")
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/health', (req: Request, res: Response) => {
-  res.json({ status: 'ok' });
+  res.json({ status: a.b });
 });
 
 app.listen(PORT, () => {
