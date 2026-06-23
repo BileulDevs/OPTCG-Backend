722160f57f1b291ae9ad3d9bea7713e08abcfecb
BileulDevs <bsahraoui.mail@gmail.com>
2026-06-23 20:12:02 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Response, Request} from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port http://localhost:${PORT}`)
+})
