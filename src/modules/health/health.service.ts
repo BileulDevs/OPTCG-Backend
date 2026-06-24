6867c0894c5b9e1780ab8e6f9872726d63c14b37
BileulDevs <bsahraoui.mail@gmail.com>
2026-06-24 02:42:37 +0200

    prettier: write

--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -1,13 +1,13 @@
 export interface HealthStatus {
-    status: "ok",
-    uptime: number,
-    timestamp: string
+  status: "ok";
+  uptime: number;
+  timestamp: string;
 }
 
-export function getHealthStatus() : HealthStatus {
-    return {
-        status: "ok",
-        uptime: Number(process.uptime().toFixed(2)),
-        timestamp: new Date().toISOString()
-    }
+export function getHealthStatus(): HealthStatus {
+  return {
+    status: "ok",
+    uptime: Number(process.uptime().toFixed(2)),
+    timestamp: new Date().toISOString(),
+  };
 }
