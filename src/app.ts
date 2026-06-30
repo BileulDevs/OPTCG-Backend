bc570cde8fb7d77b33e73afee96a5774e8faafa6
BileulDevs <bsahraoui.mail@gmail.com>
2026-06-30 03:57:09 +0200

    feat: requestId and errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import healthRoutes from "@/modules/health/health.routes";
+import { requestId } from "@/shared/middlewares/request-id";
+import { errorHandler } from "@/shared/middlewares/error-handler";
 
 export function createApp() {
   const app = express();
 
   app.disable("x-powered-by");
 
+  app.use(requestId);
+
   app.use(express.json());
 
   app.use("/health", healthRoutes);
+
+  app.use(errorHandler);
 
   return app;
 }
