b07996108c3d078cbae6ee0f24a0b2736ec7c40d
BileulDevs <bsahraoui.mail@gmail.com>
2026-06-30 03:54:35 +0200

    feat: appError class

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,41 @@
+export abstract class AppError extends Error {
+  constructor(
+    message: string,
+    public statusCode: number,
+    public code: string,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.name = new.target.name;
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message: string, code: string) {
+    super(message, 400, code);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string, code: string) {
+    super(message, 401, code);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message: string, code: string) {
+    super(message, 403, code);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string, code: string) {
+    super(message, 404, code);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message: string, code: string) {
+    super(message, 409, code);
+  }
+}
