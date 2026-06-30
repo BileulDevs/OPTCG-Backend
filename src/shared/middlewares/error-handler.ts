21303f2916e201e87433d4bd432743a436e6b7f3
BileulDevs <bsahraoui.mail@gmail.com>
2026-06-30 13:29:45 +0200

    feat: error-handler json

--- src/shared/middlewares/error-handler.ts
+++ src/shared/middlewares/error-handler.ts
@@ -1,17 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/shared/errors/AppError";
+import { config } from "@/shared/config/config";
 
 export const errorHandler = (err: unknown, req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof AppError) {
     res.status(err.statusCode).json({
-      message: err.message,
-      code: err.code,
+      error: {
+        message: err.message,
+        code: err.code,
+      },
     });
   } else {
-    console.error(err);
+    console.error({
+      requestId: req.requestId,
+      error: config.isProduction ? "Internal error" : err,
+    });
+
     res.status(500).json({
-      message: "An internal error has occured",
-      code: "INTERNAL_ERROR",
+      error: {
+        message: "An internal error has occured",
+        code: "INTERNAL_ERROR",
+      },
     });
   }
 };
